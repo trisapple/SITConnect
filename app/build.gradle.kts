378b0f580f73bcf2f99d8ab9e7de22744d984b97
Tristan Chng <40517765+trisapple@users.noreply.github.com>
2026-03-20 22:44:37 +0800

    Compatible for release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.fragment.ktx)
 
     // Firebase
     implementation(platform(libs.firebase.bom))
