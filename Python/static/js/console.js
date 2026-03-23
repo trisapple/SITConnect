cda6a90709f1da4b593f883bf313977d0db03e5f
Tristan Chng <40517765+trisapple@users.noreply.github.com>
2026-03-23 17:47:40 +0800

    More commands button

--- Python/static/js/console.js
+++ Python/static/js/console.js
@@ -217,6 +217,61 @@ function clearOutput() {
     outputDiv.innerHTML = '<div class="welcome-message"><p>Output cleared</p></div>';
 }
 
+function positionMoreCommandsMenu(detailsEl) {
+    const menuEl = detailsEl.querySelector('.suggestion-more-menu');
+    if (!menuEl) {
+        return;
+    }
+
+    const viewportPadding = 8;
+    const detailsRect = detailsEl.getBoundingClientRect();
+
+    detailsEl.classList.remove('align-left', 'align-right');
+    if (detailsRect.left > window.innerWidth / 2) {
+        detailsEl.classList.add('align-right');
+    } else {
+        detailsEl.classList.add('align-left');
+    }
+
+    detailsEl.style.setProperty('--menu-offset-x', '0px');
+
+    const menuRect = menuEl.getBoundingClientRect();
+    let shiftX = 0;
+
+    if (menuRect.right > window.innerWidth - viewportPadding) {
+        shiftX -= menuRect.right - (window.innerWidth - viewportPadding);
+    }
+
+    if (menuRect.left + shiftX < viewportPadding) {
+        shiftX += viewportPadding - (menuRect.left + shiftX);
+    }
+
+    detailsEl.style.setProperty('--menu-offset-x', `${Math.round(shiftX)}px`);
+}
+
+function setupMoreCommandsDropdown() {
+    const detailsEl = document.querySelector('.suggestion-more');
+    if (!detailsEl) {
+        return;
+    }
+
+    const reposition = () => {
+        if (detailsEl.open) {
+            positionMoreCommandsMenu(detailsEl);
+        }
+    };
+
+    detailsEl.addEventListener('toggle', () => {
+        if (detailsEl.open) {
+            requestAnimationFrame(() => positionMoreCommandsMenu(detailsEl));
+        } else {
+            detailsEl.style.setProperty('--menu-offset-x', '0px');
+        }
+    });
+
+    window.addEventListener('resize', reposition);
+}
+
 // Callback hooks for shared.js
 window.onClientConnected = (data) => {
     renderClients();
@@ -271,4 +326,6 @@ window.onPageLoad = () => {
             commandInput.focus();
         });
     });
+
+    setupMoreCommandsDropdown();
 };
